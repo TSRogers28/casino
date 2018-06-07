bc167a42cd1f7d26a470c91fbd0c4b9ca6aa8ec0
Tommy Rogers <tommyr@TommyRogers.local>
2018-06-07 17:35:11 -0400

    added dice test

--- src/main/java/io/zipcoder/casino/Threes.java
+++ src/main/java/io/zipcoder/casino/Threes.java
@@ -1,38 +1,115 @@
-package io.zipcoder.casino;
+
+    package io.zipcoder.casino;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
-public class Threes {
-    int numberOfDice;
-    int sum;
-    int rollNumber;
+    public class Threes {
 
-    int ante;
+        int sumTemp;
+        int rollNumber;
+        Player whichPlayer;
+        Player player1 = new Player();
+        Player player2 = new Player();
+        int sumPlayer1;
+        int sumPlayer2;
+        Player winner;
+        int ante;
+        int numberOfDice; // will set these up for multiple turns and gambling.
 
-    Dice dice = new Dice(5);
-
-    public Threes(){
-        rollNumber = 0;
-    }
-
-    public int roll(){
-        ArrayList<Integer> result = dice.toss();
-        Iterator<Integer>arrayIterator = result.iterator();
-
-        while(arrayIterator.hasNext()){
-            int die = result.get(arrayIterator.next());
-            if(3 == die){
-                result.set(die, 0);
-            }
-            sum += die;
-            System.out.print("\t" + result.get(arrayIterator.next()));
+        public static void main(String[] args){
+            Threes game = new Threes();
+            Player whichPlayer = game.choosePlayer();
+            //game.roll(whichPlayer);
+            game.score(whichPlayer, game.roll(whichPlayer));
+            game.compareForWinner(game.getSumPlayer1(), game.getSumPlayer2());
         }
-        rollNumber++;
-        System.out.print("\t Your roll #" + rollNumber + " was " + sum);
 
-        return sum;
+
+        Dice dice = new Dice(5);
+
+        public Threes(){
+            rollNumber = 0;
+        }
+
+        public Player getWinner(){
+            return winner;
+        }
+
+        public void setWinner(Player winner){
+            this.winner = winner;
+        }
+
+        public int getSumPlayer1(){
+            return sumPlayer1;
+        }
+
+        public int getSumPlayer2(){
+            return sumPlayer2;
+        }
+
+        public void setSumTemp(int sumTemp){
+            this.sumTemp = sumTemp;
+        }
+
+        public Player choosePlayer(){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Who wants to start?");
+            String pickedPlayer = scan.next();
+            whichPlayer = (pickedPlayer.equals(player1)) ? player1 : player2;
+            return whichPlayer;
+        }
+
+        io.zipcoder.casino.Threes game = new io.zipcoder.casino.Threes();
+
+
+        public int roll(Player whichPlayer){
+            ArrayList<Integer> result = dice.toss();
+            Iterator<Integer>arrayIterator = result.iterator();
+
+            while(arrayIterator.hasNext()){
+                int die = result.get(arrayIterator.next());
+                if(3 == die){
+                    die = 0;
+                }
+                sumTemp += die;
+                System.out.print("\t" + result.get(arrayIterator.next()));
+            }
+            return sumTemp;
+        }
+
+        public void score(Player whichPlayer, int sumTemp) {
+            while (rollNumber <= 1) {
+                if (whichPlayer == player1) {
+                    sumPlayer1 = sumTemp;
+                    System.out.print("\t Your roll #" + rollNumber + " was " + sumPlayer1);
+                    setSumTemp(0);
+                    roll(player2);
+                    rollNumber++;
+                } else {
+                    sumPlayer2 = sumTemp;
+                    System.out.print("\t Your roll #" + rollNumber + " was " + sumPlayer2);
+                    setSumTemp(0);
+                    roll(player1);
+                    rollNumber++;
+                }
+            }
+        }
+
+        public void compareForWinner(int sumPlayer1, int sumPlayer2){
+            Player winner = (sumPlayer1 < sumPlayer2) ? player1 : player2;
+            System.out.println(winner+ " wins!");
+            game.setWinner(winner);
+        }
+
+//        public void getOut(){
+//
+//        } This will return back into the casino when ready;
+
+
     }
 
 
-}
+
+
