966c19c5f2f7d2476b364f098b26073e290868e1
Tommy Rogers <tommyr@TommyRogers.local>
2018-06-08 09:39:26 -0400

    turning threes to one player

--- src/main/java/io/zipcoder/casino/Game.java
+++ src/main/java/io/zipcoder/casino/Game.java
@@ -4,20 +4,10 @@ package io.zipcoder.casino;
 
 interface Game {
 
-<<<<<<< HEAD
-    //void nextTurn();
 
     void playGame();
 
     Player getWinner();
 
-    //boolean isOver();
-
-=======
-
-    void playGame();
-
-    Player getWinner();
->>>>>>> d4eea0afe8484d22cb5d6534ff9d8aa8ac168b40
 
 }
