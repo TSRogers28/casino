f7ea235b8478685e31fad97cdb132e0e635284a7
David Thornley <davidT@Davids-zipcode-MacBook.local>
2018-06-09 16:21:52 -0400

    fully functional baccarat game

--- src/test/java/io/zipcoder/casino/ThreesTest.java
+++ src/test/java/io/zipcoder/casino/ThreesTest.java
@@ -6,11 +6,9 @@ import org.junit.Test;
 public class ThreesTest {
 
 
-<<<<<<< HEAD
+
    // private Player player1;
-=======
-    //private Player player1;
->>>>>>> d7c0d591ba2e8bacb02592a922957271ee26cd4f
+
 
 
 
@@ -30,7 +28,7 @@ public class ThreesTest {
     public void score() {
     }
 
-<<<<<<< HEAD
+
     /*@Test
     public void compareForWinner() {
         Threes tester = new Threes();
@@ -42,18 +40,5 @@ public class ThreesTest {
         assertEquals(actual, expected);
         
     }*/
-=======
-//    @Test
-//    public void compareForWinner() {
-//        Threes tester = new Threes();
-//        int sumPlayer1 = 15;
-//        int sumPlayer2 = 20;
-//
-//        Player expected = player1;
-//        Player actual = tester.compareForWinner(sumPlayer1, sumPlayer2);
-//        assertEquals(actual, expected);
-//
-//    }
->>>>>>> d7c0d591ba2e8bacb02592a922957271ee26cd4f
 
 }
